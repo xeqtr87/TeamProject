2573f312aa6f0367f97cf7659d61199c6ef934b0
Johan Stening <johan.stening@hotmail.com>
2016-03-26 15:50:05 +0100

    Setting Things up from templates

---
+++ Ali/TravelBooking/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TravelBooking.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Ali/TravelBooking/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelBooking.Startup))]
+namespace TravelBooking
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
