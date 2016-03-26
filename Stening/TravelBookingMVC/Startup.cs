165c568e66c8d59334d9330b2362f82bcb9b247f
Johan Stening <johan.stening@hotmail.com>
2016-03-26 17:50:30 +0100

    added entity data model + other stuff

---
+++ Stening/TravelBookingMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelBookingMVC.Startup))]
+namespace TravelBookingMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
